d2f2b3dc12f9571072dcf480eaf74977e31c5f21
zbz <nnzbz@21cn.com>
2024-01-25 14:14:22 +0800

    + 添加Vue3

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+    // 项目根目录(index.html所在的目录)
+    root: resolve(__dirname, 'src', 'renderer'),
+    plugins: [vue()],
+});
