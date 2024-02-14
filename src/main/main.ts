803cc5b9ab8018ae6b4452364c85d8506b17a624
zbz <nnzbz@21cn.com>
2024-02-14 20:32:40 +0800

    * 不完美的解决不能启动的问题

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,6 +1,5 @@
 import { app, BrowserWindow, autoUpdater, dialog, MessageBoxOptions } from 'electron';
 import path from 'path';
-import { default as electronSquirrelStartup } from 'electron-squirrel-startup';
 // import { updateElectronApp } from 'update-electron-app';
 // import Logger from 'electron-log';
 
@@ -42,7 +41,7 @@ if (!isDev) {
 }
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (electronSquirrelStartup) {
+if (require('electron-squirrel-startup')) {
     app.quit();
 }
 
